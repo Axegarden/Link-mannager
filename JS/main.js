73789e411965356f8845385dbbbc9598d86235aa
Simon <Simerboy@users.noreply.github.com>
2025-04-02 10:49:04 +0200

    Add link validation and alert for unavailable redirects in main.js

--- JS/main.js
+++ JS/main.js
@@ -2,12 +2,18 @@
 document.getElementById('button').addEventListener('click', openLink);
 
 //change the redirect link here
-const link = 'https://www.google.com';
+const link = '';
 
 // Open a new link when the page is loaded
 window.onload = function() {
-    window.open(link, '_blank');
-    window.close();
+    if (link==''){
+        alert('beklager. der er ingen links tilgængelige lige nu.');
+        window.close();
+    }
+    else{
+        window.open(link, '_blank');
+        window.close();
+    }
 };
 // Open a new link when the button is clicked
 function openLink() {
