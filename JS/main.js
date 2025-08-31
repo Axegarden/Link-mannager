a5a3837cbe449b2f7c43c9809581fc57d1022231
Axegarden <110640042+Axegarden@users.noreply.github.com>
2025-08-31 15:02:46 +0200

    .

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,28 @@
 //runs openLink() when the button is clicked
-document.getElementById('button').addEventListener('click', openLink);
+document.getElementById('button1').addEventListener('click', openLink1);
+document.getElementById('button2').addEventListener('click', openLink2);
+document.getElementById('button3').addEventListener('click', openLink3);
 
 //change the redirect link here
-const link = '';
+const link1 = 'https://www.youtube.com';
+const link2 = 'https://www.google.com';
+const link3 = 'https://www.netflix.com';
 
-// Open a new link when the page is loaded
-window.onload = function() {
-    if (link==''){
-        alert('beklager. der er ingen links tilgængelige lige nu.');
-        window.close();
-    }
-    else{
-        window.open(link, '_blank');
-        window.close();
-    }
-};
 // Open a new link when the button is clicked
-function openLink() {
-    window.open(link, '_blank');
+function openLink1() {
+    alert("Du bliver nu viderestillet til: " + link1);
+    window.open(link1, "_blank");
+    window.close();
+}
+
+function openLink2() {
+    alert("Du bliver nu viderestillet til: " + link2);
+    window.open(link2, "_blank");
+    window.close();
+}
+
+function openLink3() {
+    alert("Du bliver nu viderestillet til: " + link3);
+    window.open(link3, "_blank");
     window.close();
 }
