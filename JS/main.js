61533345eac281506e1ca0ce2c54e1ad218d2f25
Simon <110640042+Axegarden@users.noreply.github.com>
2025-09-02 12:30:25 +0200

    updated links

--- JS/main.js
+++ JS/main.js
@@ -4,24 +4,36 @@ document.getElementById('button2').addEventListener('click', openLink2);
 document.getElementById('button3').addEventListener('click', openLink3);
 
 //change the redirect link here
-const link1 = 'https://www.youtube.com';
-const link2 = 'https://www.google.com';
-const link3 = 'https://www.netflix.com';
+const link1 = '';
+const link2 = 'https://discord.gg/TEK9BtEtkC';
+const link3 = '';
 
 // Open a new link when the button is clicked
 function openLink1() {
+    if (link1 == ''){
+        alert('denne knap er midlertidigt deaktiveret');
+        return;
+    }
     alert("Du bliver nu viderestillet til: " + link1);
     window.open(link1, "_blank");
     window.close();
 }
 
 function openLink2() {
+    if (link2 == ''){
+        alert('denne knap er midlertidigt deaktiveret');
+        return;
+    }
     alert("Du bliver nu viderestillet til: " + link2);
     window.open(link2, "_blank");
     window.close();
 }
 
 function openLink3() {
+    if (link3 == ''){
+        alert('denne knap er midlertidigt deaktiveret');
+        return;
+    }
     alert("Du bliver nu viderestillet til: " + link3);
     window.open(link3, "_blank");
     window.close();
