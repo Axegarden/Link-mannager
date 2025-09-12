ac146c146038ffaac3b125caf7d9443454a45029
Simon <110640042+Axegarden@users.noreply.github.com>
2025-09-12 09:50:40 +0200

    added a button and fixed some links

--- JS/main.js
+++ JS/main.js
@@ -2,16 +2,19 @@
 document.getElementById('button1').addEventListener('click', openLink1);
 document.getElementById('button2').addEventListener('click', openLink2);
 document.getElementById('button3').addEventListener('click', openLink3);
+document.getElementById('button4').addEventListener('click', openLink4);
 
 //change the redirect link here
+//if the link is empty, the button will be disabled
 const link1 = '';
-const link2 = 'https://discord.gg/TEK9BtEtkC';
-const link3 = '';
+const link2 = '';
+const link3 = 'https://discord.gg/TEK9BtEtkC';
+const link4 = 'https://www.instagram.com/lanudvalget_htxsesg/';
 
 // Open a new link when the button is clicked
 function openLink1() {
     if (link1 == ''){
-        alert('denne knap er midlertidigt deaktiveret');
+        alert('Denne knap er midlertidigt deaktiveret');
         return;
     }
     alert("Du bliver nu viderestillet til: " + link1);
@@ -21,7 +24,7 @@ function openLink1() {
 
 function openLink2() {
     if (link2 == ''){
-        alert('denne knap er midlertidigt deaktiveret');
+        alert('Denne knap er midlertidigt deaktiveret');
         return;
     }
     alert("Du bliver nu viderestillet til: " + link2);
@@ -31,10 +34,20 @@ function openLink2() {
 
 function openLink3() {
     if (link3 == ''){
-        alert('denne knap er midlertidigt deaktiveret');
+        alert('Denne knap er midlertidigt deaktiveret');
         return;
     }
     alert("Du bliver nu viderestillet til: " + link3);
     window.open(link3, "_blank");
+    window.close();
+}
+
+function openLink4() {
+    if (link4 == ''){
+        alert('Denne knap er midlertidigt deaktiveret');
+        return;
+    }
+    alert("Du bliver nu viderestillet til: " + link4);
+    window.open(link4, "_blank");
     window.close();
 }
